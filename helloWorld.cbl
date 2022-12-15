b5420914e1ec832feffaaa95d6d5768aec87ff0a
waritpark <lafarge21@hotmail.fr>
2022-12-16 00:03:02 +0100

    initialisation du projet

---
+++ helloWorld.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Arthur Lafarge
+      * Date: 15/12/2022
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Hello.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM Hello.
